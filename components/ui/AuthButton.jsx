ece7f2819364e7685f4b9b9ae70c1dacbe1dcc49
vvk079 <vivekkushwah079@gmail.com>
2026-02-18 01:00:19 +0530

    Fix sign-in/sign-out issues and add root middleware

--- components/ui/AuthButton.jsx
+++ components/ui/AuthButton.jsx
@@ -3,39 +3,39 @@ import React from 'react'
 import { Button } from './button'
 import { Form, LogIn, LogOut } from 'lucide-react'
 import { useState } from 'react'
-import AuthModel from './AuthModel'
+import AuthModal from './AuthModel'
 import { signOut } from '@/app/Action'
-const AuthButton = ({user}) => {
+const AuthButton = ({ user }) => {
 
 
-    const[showAuthModel,setShowAuthModel]=useState(false)
+  const [showAuthModel, setShowAuthModel] = useState(false)
 
-    if(user){
+  if (user) {
 
-        return (
-    <form action={signOut}>
-      <Button variant="ghost" size="sm" type="submit" className="gap-2">
-        <LogOut className="h-4 w-4" />
-        Sign Out
-      </Button>
-    </form>
-  );
-    }
+    return (
+      <form action={signOut}>
+        <Button variant="ghost" size="sm" type="submit" className="gap-2">
+          <LogOut className="h-4 w-4" />
+          Sign Out
+        </Button>
+      </form>
+    );
+  }
   return (
     <>
-    <Button variant="default" size="sm"
-            className="bg-pink-500 hover:bg-pink-600 gap-2"
-            onClick={()=>setShowAuthModel(true)}
-          >
-            <LogIn className="h-4 w-4" />
+      <Button variant="default" size="sm"
+        className="bg-pink-500 hover:bg-pink-600 gap-2"
+        onClick={() => setShowAuthModel(true)}
+      >
+        <LogIn className="h-4 w-4" />
 
-            Sign In</Button>
+        Sign In</Button>
 
 
-            <AuthModel
-            isOpen={showAuthModel}
-            onClose={()=>setShowAuthModel(false)}
-            />
+      <AuthModel
+        open={showAuthModel}
+        onClose={() => setShowAuthModel(false)}
+      />
     </>
   )
 }
